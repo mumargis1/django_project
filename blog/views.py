1019219545539aa214740a8eeee419e6f3f77aab
mumar <mumar@cybershellsol.com>
2022-07-25 19:43:48 +0500

    upto register-crispy

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from sympy import content
-
-posts = [
-    {
-        'author':'author1', 
-        'title':'Blog post 1', 
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author':'author2',
-        'title':'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'Augest 28, 2018'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
     }
     # return HttpResponse('<h1>Blog Home </h1>')
     return render(request, 'blog/home.html', context)
--- django_project/settings.py
+++ django_project/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
